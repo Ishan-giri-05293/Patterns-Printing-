27239be685edf6047d3d3e84d82e409ae5cc1c1b
Ishan Giri <84699867+Ishan-giri-05293@users.noreply.github.com>
2025-09-05 07:33:46 +0530

    Add files via upload

---
+++ Right-Angled Number Pyramid.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+void numbers()
+{
+    for (int i = 1; i <= 6; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    numbers();
+    return 0;
+}
