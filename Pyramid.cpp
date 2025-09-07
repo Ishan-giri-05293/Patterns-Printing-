677425bf2d9f6b64db028a0a77dd16ab4540fa51
Ishan Giri <84699867+Ishan-giri-05293@users.noreply.github.com>
2025-09-07 22:42:22 +0530

    Add files via upload

---
+++ Pyramid.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void pattern7(int N)
+{
+    for (int i = 0; i < N; i++)
+    {
+        // Print (N-i-1) spaces + (2*i+1) stars in one line
+        cout << string(N - i - 1, ' ') << string(2 * i + 1, '*') << endl;
+    }
+}
+
+int main()
+{
+    int N = 5;
+    pattern7(N);
+    return 0;
+}
